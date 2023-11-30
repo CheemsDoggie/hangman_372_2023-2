25c717d978b7562b7e0fb399f7a11b03dcd05f8c
unknown <Kyonyx>
2023-11-30 00:38:43 -0800

    Agrege el monito hangman en ASCII, agrege codigo para saber si ya se ha introducido un caracter anteriormente, cambie el nombre de words.py a resources.py ya que en ese archivo tambien se guarda el array contendor del hangman ASCII

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
 import string
 
-from words import palabras
+from resources import palabras
+from resources import hangmanASCI
 
 def welcome():
     print("Welcome to the hangman game in Python")
@@ -13,7 +14,7 @@ def get_valid_word(words):
     return word.upper()  # Convertir la palabra a mayúsculas
 
 def display_word(word, guessed_letters):
-    display = ''
+    display = 'Word to guess: '
     for letter in word:
         if letter in guessed_letters:
             display += letter + ' '
@@ -30,9 +31,13 @@ def hangman():
     guessed_letters = set()
     lives = 6
 
-    print("Word to guess:", display_word(word_to_guess, guessed_letters))
-
     while lives > 0:
+        # Monito Hangaman en ASCI
+        print(hangmanASCI[lives]);
+
+        current_display = display_word(word_to_guess, guessed_letters)
+        print(current_display)
+
         guess = input("Guess a letter: ").upper()  # Convertir la letra a mayúsculas
 
         if guess in alphabet - guessed_letters:
@@ -40,18 +45,19 @@ def hangman():
             if guess not in word_letters:
                 lives -= 1
 
-            current_display = display_word(word_to_guess, guessed_letters)
-            print(current_display)
-
             if set(word_letters).issubset(guessed_letters):
+                print(hangmanASCI[lives]);
                 print("Congratulations! You guessed the word:", word_to_guess)
                 break
-        else:
+        elif guess in guessed_letters:
+            print("You have already guessed that letter, try again.")
+        else: 
             print("Invalid input. Please enter a valid letter.")
 
         print("Lives left:", lives)
 
         if lives == 0:
+            print(hangmanASCI[lives]);
             print("Sorry, you ran out of lives. The word was:", word_to_guess)
 
 if __name__ == "__main__":
