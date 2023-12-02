51f9750b0ddf698ced3d56f3f861fda2bc2f4b5b
AdanEscamilla <adan.escamilla@uabc.edu.mx>
2023-12-01 18:11:09 -0800

    Adan Escamilla: hice que el juego se repita hasta que el usuario decida que ya no quiere jugar

--- main.py
+++ main.py
@@ -27,45 +27,50 @@ def display_incorrect_guesses(incorrect_guesses):
 
 
 def hangman():
-    welcome()
+    while True:
+        welcome()
 
-    word_to_guess = get_valid_word(palabras)
-    word_letters = set(word_to_guess)
-    alphabet = set(string.ascii_uppercase)
-    guessed_letters = set()
-    incorrect_guesses = set()
-    lives = 6
+        word_to_guess = get_valid_word(palabras)
+        word_letters = set(word_to_guess)
+        alphabet = set(string.ascii_uppercase)
+        guessed_letters = set()
+        incorrect_guesses = set()
+        lives = 6
 
-    while lives > 0:
-        # Monito Hangaman en ASCI
-        print(hangmanASCI[lives]);
+        while lives > 0:
+            # Monito Hangaman en ASCI
+            print(hangmanASCI[lives])
 
-        current_display = display_word(word_to_guess, guessed_letters)
-        print(current_display)
-        print(display_incorrect_guesses(incorrect_guesses))
+            current_display = display_word(word_to_guess, guessed_letters)
+            print(current_display)
+            print(display_incorrect_guesses(incorrect_guesses))
 
-        guess = input("Guess a letter: ").upper()  # Convertir la letra a mayúsculas
+            guess = input("Guess a letter: ").upper()  # Convertir la letra a mayúsculas
 
-        if guess in alphabet - guessed_letters:
-            guessed_letters.add(guess)
-            if guess not in word_letters:
-                lives -= 1
-                incorrect_guesses.add(guess)
+            if guess in alphabet - guessed_letters:
+                guessed_letters.add(guess)
+                if guess not in word_letters:
+                    lives -= 1
+                    incorrect_guesses.add(guess)
 
-            if set(word_letters).issubset(guessed_letters):
-                print(hangmanASCI[lives]);
-                print("Congratulations! You guessed the word:", word_to_guess)
-                break
-        elif guess in guessed_letters:
-            print("You have already guessed that letter, try again.")
-        else: 
-            print("Invalid input. Please enter a valid letter.")
+                if set(word_letters).issubset(guessed_letters):
+                    print(hangmanASCI[lives])
+                    print("Congratulations! You guessed the word:", word_to_guess)
+                    break
+            elif guess in guessed_letters:
+                print("You have already guessed that letter, try again.")
+            else: 
+                print("Invalid input. Please enter a valid letter.")
 
-        print("Lives left:", lives)
+            print("Lives left:", lives)
 
-        if lives == 0:
-            print(hangmanASCI[lives]);
-            print("Sorry, you ran out of lives. The word was:", word_to_guess)
+            if lives == 0:
+                print(hangmanASCI[lives])
+                print("Sorry, you ran out of lives. The word was:", word_to_guess)
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            break               
 
 if __name__ == "__main__":
     hangman()
